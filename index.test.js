9782e51895f3ae30de9115ef01138869df5c9faa
Jason Haire <cjhaire@student.fullsail.edu>
2025-11-21 10:02:03 -0500

    added requirements

---
+++ index.test.js
@@ -0,0 +1,16 @@
+//Here are the main functions we are pulling to test. The functions we have are as follows: 
+/*
+requirements: 
+  You must implement at least four (4) of the functions listed in the table below. Each function is designed to test your understanding of JavaScript concepts, including loops, conditionals, and string/array manipulation.
+
+  Here are the main functions we are pulling to test. The functions we have are as follows:
+    sumOfArray(arr)
+    reverseString(str)
+    isPalindrome(str)
+    findMax(arr)
+    capitalizeWords(str)
+    countVowels(str)
+    purgeDuplicates(arr)
+  
+  To add a bit of a challenge, there's a plot twist for this week's activity. In addition to implementing 4 of the functions we've discussed, you'll also need to write functional tests for each of the 4 functions you've created. This was mentioned at the beginning of the assignment, so keep that in mind.
+*/
